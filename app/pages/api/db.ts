098a48aa71b35fa75cbd2aca35bd4e0f05c67103
ColinKwiecinski <colinkwi@uw.edu>
2022-05-03 20:43:19 -0700

    add draft interface for keyword row

--- app/pages/api/db.ts
+++ app/pages/api/db.ts
@@ -3,6 +3,22 @@
  */
 const sql = require('mssql')
 
+export interface KeywordResult {
+    KeywordID: number,
+    KeyName: string,
+    KeyDesc: string,
+    KeyLink: string,
+    // Keytags: string[], // If tags are gonna be an array, uncomment this and remove the other tags below
+    KeyTag1: string,
+    KeyTag2: string,
+    KeyTag3: string,
+    KeyTag4: string,
+    Industry1: string,
+    Industry2: string
+    Importance: number
+}
+
+
 // For local testing, make sure you export these env variables in your own terminal. check vercel for all the values
 const sqlConfig = {
     user: process.env.DB_USER,
