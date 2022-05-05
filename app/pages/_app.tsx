e6107351dcd3897f7f17722fb9acdc9d6314d814
ColinKwiecinski <colinkwi@uw.edu>
2022-05-04 18:15:25 -0700

    update theme

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -5,6 +5,10 @@ import { ThemeProvider } from 'next-themes';
 function MyApp({ Component, pageProps }: AppProps) {
   globalStyles();
   return <ThemeProvider
+    // disableTransitionOnChange
+    // attribute="class"
+    // value={{ light: 'light-theme', dark: darkTheme.className }}
+    // defaultTheme="light"
   >
     <Component {...pageProps} />
   </ThemeProvider>
